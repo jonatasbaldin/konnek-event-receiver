783fd499eeb680ccf3387b85038da90a6f2e6858
Jonatas Baldin <jonatas.baldin@gmail.com>
2020-04-25 20:05:42 +0200

    Simplify knative-receiver

--- go.mod
+++ go.mod
@@ -3,13 +3,6 @@ module github.com/konnek/knative-receiver
 go 1.13
 
 require (
-	contrib.go.opencensus.io/exporter/stackdriver v0.13.1 // indirect
-	contrib.go.opencensus.io/exporter/zipkin v0.1.1 // indirect
-	github.com/aws/aws-lambda-go v1.16.0 // indirect
-	github.com/cloudevents/sdk-go v1.1.2
+	github.com/cloudevents/sdk-go v1.2.0
 	github.com/kelseyhightower/envconfig v1.4.0
-	github.com/knative/eventing-sources v0.13.3 // indirect
-	k8s.io/api v0.18.0 // indirect
-	knative.dev/eventing v0.13.5
-	knative.dev/pkg v0.0.0-20200402224918-0cf29f826c40 // indirect
 )
--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 
 	cloudevents "github.com/cloudevents/sdk-go"
 	"github.com/kelseyhightower/envconfig"
-	"knative.dev/eventing/pkg/kncloudevents"
 )
 
 type EnvConfig struct {
+	Port        int    `envconfig:"PORT" default:"8080"`
 	Sink        string `envconfig:"K_SINK"`
 	CEOverrides string `envconfig:"K_CE_OVERRIDES"`
 }
@@ -17,31 +17,36 @@ type EnvConfig struct {
 var sink string
 
 func receiveAndForward(event cloudevents.Event) {
-	client, err := kncloudevents.NewDefaultClient(sink)
+	transport, err := cloudevents.NewHTTPTransport(cloudevents.WithTarget(sink))
 	if err != nil {
-		log.Fatalf("could not create forwarder client: %v\n", err)
+		log.Fatalf("could not create transport: %v", err)
+	}
+
+	client, err := cloudevents.NewClient(transport)
+	if err != nil {
+		log.Fatalf("could not create client: %v", err)
 	}
 
 	ctx := context.Background()
 
 	_, _, err = client.Send(ctx, event)
 	if err != nil {
-		log.Fatalf("could not send event: %v\n", err)
+		log.Fatalf("could not send event: %v", err)
 	}
 }
 
 func main() {
-	var envConfig EnvConfig
-	err := envconfig.Process("", &envConfig)
+	var env EnvConfig
+	err := envconfig.Process("", &env)
 	if err != nil {
-		log.Fatalf("could not load environment variables: %v\n", err)
+		log.Fatalf("could not load environment variables: %v", err)
 	}
 
-	sink = envConfig.Sink
+	sink = env.Sink
 
-	client, err := kncloudevents.NewDefaultClient()
+	client, err := cloudevents.NewDefaultClient()
 	if err != nil {
-		log.Fatalf("could not create client: %v\n", err)
+		log.Fatalf("could not create client: %v", err)
 	}
 
 	ctx := context.Background()
@@ -51,5 +56,5 @@ func main() {
 		log.Fatalf("failed to start receiver, %v", err)
 	}
 
-	log.Printf("listening on port %d\n", 8080)
+	log.Printf("listening on port %d", env.Port)
 }
